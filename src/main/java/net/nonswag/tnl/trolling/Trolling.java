432f6795d9bf9375490768500ed1c96410154363
NonSwag <mrminecraft00@gmail.com#>
2021-09-12 18:04:55 +0200

    update

--- src/main/java/net/nonswag/tnl/trolling/Trolling.java
+++ src/main/java/net/nonswag/tnl/trolling/Trolling.java
@@ -2,12 +2,12 @@ package net.nonswag.tnl.trolling;
 
 import net.nonswag.tnl.listener.api.command.CommandManager;
 import net.nonswag.tnl.listener.api.event.EventManager;
+import net.nonswag.tnl.listener.api.plugin.TNLPlugin;
 import net.nonswag.tnl.trolling.commands.TrollCommand;
 import net.nonswag.tnl.trolling.listeners.JoinListener;
 import net.nonswag.tnl.trolling.listeners.PacketListener;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class Trolling extends JavaPlugin {
+public class Trolling extends TNLPlugin {
 
     @Override
     public void onEnable() {
