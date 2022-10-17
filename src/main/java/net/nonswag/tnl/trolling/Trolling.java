68ac749209bc88dfef70328fa66ccd1730e9854e
NonSwag <mrminecraft00@gmail.com>
2022-10-17 20:43:27 +0200

    added auto updater

--- src/main/java/net/nonswag/tnl/trolling/Trolling.java
+++ src/main/java/net/nonswag/tnl/trolling/Trolling.java
@@ -1,6 +1,8 @@
 package net.nonswag.tnl.trolling;
 
+import net.nonswag.tnl.listener.api.plugin.PluginUpdate;
 import net.nonswag.tnl.listener.api.plugin.TNLPlugin;
+import net.nonswag.tnl.listener.api.settings.Settings;
 import net.nonswag.tnl.listener.api.version.Version;
 import net.nonswag.tnl.trolling.commands.TrollCommand;
 import net.nonswag.tnl.trolling.listeners.ConnectionListener;
@@ -13,5 +15,8 @@ public class Trolling extends TNLPlugin {
         getCommandManager().registerCommand(new TrollCommand());
         getEventManager().registerListener(Version.v1_16_4, PacketListener::new);
         getEventManager().registerListener(new ConnectionListener());
+        async(() -> {
+            if (Settings.AUTO_UPDATER.getValue()) new PluginUpdate(this).downloadUpdate();
+        });
     }
 }
